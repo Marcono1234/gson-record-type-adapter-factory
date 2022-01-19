872f229a3c950d0d932a6032ae5670d031b73cfb
Marcono1234 <Marcono1234@users.noreply.github.com>
2022-01-19 14:13:28 +0100

    Suppress javac module name warning

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@
  * Provides the Gson type adapter factory {@link marcono1234.gson.recordadapter.RecordTypeAdapterFactory} which
  * adds JSON serialization and deserialization support for Record classes (Java 16 feature).
  */
-@SuppressWarnings("JavaModuleNaming")
+// Suppress warning for module name ending with digits, see also https://bugs.openjdk.java.net/browse/JDK-8264488
+// (uses javac and IntelliJ warning names)
+@SuppressWarnings({"module", "JavaModuleNaming"})
 module marcono1234.gson.recordadapter {
     requires transitive com.google.gson;
 
